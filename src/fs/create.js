8ba5a31e379abf11dbe1ccd143a55a32528e8b9d
IngaMuse <ingamoshkareva@gmail.com>
2024-10-01 21:38:28 +0300

    feat: add path meta.url create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,7 +1,10 @@
 import { access, writeFile } from "node:fs/promises";
 import path from "path";
+import { fileURLToPath } from "url";
 
-const filePath = path.resolve("src", "fs", "files", "fresh.txt");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.resolve(__dirname, "files", "fresh.txt");
 const content = "I am fresh and young";
 
 const create = async () => {
