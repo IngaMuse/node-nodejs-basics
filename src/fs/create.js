a8ef1a0b9fc1a7fe3f371bbce3c8a5995197f855
IngaMuse <ingamoshkareva@gmail.com>
2024-10-01 15:10:28 +0300

    feat:add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { access, writeFile } from "node:fs/promises";
+
+const filePath = "src/fs/files/fresh.txt";
+const content = "I am fresh and young";
+
 const create = async () => {
-    console.log("start")
+  await access(filePath).then(
+    () => {
+      throw new Error("FS operation failed");
+    },
+    async () => await writeFile(filePath, content)
+  );
 };
 
 await create();
