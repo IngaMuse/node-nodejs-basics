c4f91f60dbeff9af71795dd92dc5194eb845e5b2
IngaMuse <ingamoshkareva@gmail.com>
2024-10-02 19:52:56 +0300

    feat: add read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { createReadStream } from "node:fs";
+import { pipeline } from "stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const readFile = path.resolve(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(readFile);
+  await pipeline(stream, process.stdout);
 };
 
 await read();
