b2cd2fea5397a57438ccfdccde14eeea935fe13d
IngaMuse <ingamoshkareva@gmail.com>
2024-10-02 20:01:51 +0300

    feat: add write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { createWriteStream } from "node:fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const writeFile = path.resolve(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(writeFile, "utf-8");
+  process.stdin.pipe(stream);
 };
 
 await write();
