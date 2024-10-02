c7a350f6b52868399dde6c2a8cfcde2f94fd940b
IngaMuse <ingamoshkareva@gmail.com>
2024-10-02 12:35:34 +0300

    feat: add env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const { env } = process;
+
+  const result = Object.keys(env)
+    .filter((key) => key.startsWith("RSS_"))
+    .map((key) => `${key}=${env[key]}`)
+    .join("; ");
+
+    console.log(result);
 };
 
 parseEnv();
