0f3c6f1a38a6c9587d78af50fa8984cf7585f064
IngaMuse <ingamoshkareva@gmail.com>
2024-10-02 21:09:49 +0300

    feat: add transform.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 const readFile = path.resolve(__dirname, "files", "fileToRead.txt");
 
 const read = async () => {
-  const stream = createReadStream(readFile);
+  const stream = createReadStream(readFile, "utf-8");
   await pipeline(stream, process.stdout);
 };
 
