2249862a8847a20df29aaadd9cc71612c9eaff62
IngaMuse <ingamoshkareva@gmail.com>
2024-10-03 14:55:52 +0300

    feat: add os.eol in read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,6 +2,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { createReadStream } from "node:fs";
 import { pipeline } from "stream/promises";
+import os from "os"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,7 +11,7 @@ const readFile = path.resolve(__dirname, "files", "fileToRead.txt");
 const read = async () => {
   const stream = createReadStream(readFile, "utf-8");
   await pipeline(stream, process.stdout, {end: false});
-  console.log('\n');
+  console.log(os.EOL);
 };
 
 await read();
