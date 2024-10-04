cb6bf028a9a4b3b48c4a0d05da679f4a9c7d45df
IngaMuse <ingamoshkareva@gmail.com>
2024-10-04 10:00:56 +0300

    feat: add cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { fork } from "node:child_process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const scriptFile = path.resolve(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const forked = fork(scriptFile, args);
+
+  forked.on("message", (msg) => {
+    console.log("Message from child", msg);
+  });
+  forked.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["a", "b", "c"]);
